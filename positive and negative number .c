21b37797fc91afe6e9794c7b49989fb82be9635c
rishabhsyadav <rishabhsryadav@gmail.com>
2025-07-24 14:43:10 +0530

    positive and negative number .c

---
+++ positive and negative number .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+    printf("enter the number :");
+    scanf ("%d\n" , &num);
+    if (num>0)
+        printf("its a positive number \n");
+        else
+            scanf ("its a negative number \n");
+    printf("Hello world!\n");
+    return 0;
+}
